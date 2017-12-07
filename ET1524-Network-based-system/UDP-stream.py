ec9993780ff841946e1d38387586f4fd01f2c163
Ångbåt <felix.kozma@gmail.com>
2017-12-07 11:58:05 +0100

    nu kan man säga hur många utskick per sec

--- ET1524-Network-based-system/UDP-stream.py
+++ ET1524-Network-based-system/UDP-stream.py
@@ -1,6 +1,24 @@
 from socket import *
 import time
 
+# get nr of udp utskick per sec
+sentence = 'help';
+while sentence == 'help':
+
+    sentence = input('type help for help\ninput how many udp to send per second: ')
+    if sentence == 'help':
+        print('type in a nr\nex \'50\'\ne=empty\ntype \'low\' for 25 and \'high\' for 50')
+    if (sentence == 'd') or (sentence == 'low'):
+        sentence = "25"
+    if sentence == 'high':
+        sentence = '50'
+    if sentence != 'help':
+        try:
+            nrPerSec = int(sentence)
+        except:
+            sentence = 'help'
+
+
 host = '192.168.43.108'
 port = 12000
 serverPort = 12001
